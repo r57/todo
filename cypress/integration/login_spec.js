000b1138b0ef5c7ed9a44ee5154d2d43137ce3c3
Vaclav Pospisil <pospisil.vaclav@gmail.com>
2020-03-16 12:33:26 +0100

    tests setup

---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,66 @@
+const loginUrl = 'localhost:3000'
+const validEmail = 'dude@gmail.com'
+const invalidEmail = 'dude2@gmail.com'
+const incorrectPassword = 'jirkakara'
+
+const emailInputSelector = 'label:contains("Email")~div>input'
+const submitButtonSelector = 'span:contains("Submit")'
+const mainContentSelector = '[class*="content-container"]'
+
+
+describe('Log-in to the app', function () {
+
+    it('Go to login page', function () {
+        cy.visit(loginUrl)
+        cy.url()
+            .should('include',loginUrl)
+    })
+
+    it('Input a valid email validEmail', function () {
+        cy.get(emailInputSelector)
+            .clear()
+            .type(validEmail)
+            .should('have.value',validEmail)
+    })
+
+    it('Submit the login info', function () {
+        cy.get(submitButtonSelector)
+            .click()
+    })
+
+    it('User is successfully logged in and is redirected to the mainpage', function () {
+        cy.get(mainContentSelector).should(($div) => {
+            expect($div.get(0).innerText).to.eq('This is a home component')
+        })
+    })
+})
+
+
+describe('Use non-whitelisted email for login', function () {
+
+    it('Go to login page', function () {
+        cy.visit(loginUrl)
+        cy.url()
+            .should('include',loginUrl)
+    })
+
+    it('Input an invalid email: #invalidEmail', function () {
+        cy.get(emailInputSelector)
+            .clear()
+            .type(invalidEmail)
+            .should('have.value',invalidEmail)
+    })
+
+    it('Submit the login info and get rekt with an alert window', function () {
+        const stub = cy.stub()
+
+        cy.on ('window:alert', stub)
+        cy.get(submitButtonSelector)
+            .click()
+            .then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('Wrong password dude/et')      
+            })  
+    })
+
+})
+
