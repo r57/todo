6d728b7969c4c9ce5413215710458771a4a4e9c6
vpos2 <vpospisil@fevo.com>
2020-03-16 19:34:54 +0100

    add task

--- cypress/integration/ui/tasks_spec.js
+++ cypress/integration/ui/tasks_spec.js
@@ -4,13 +4,21 @@ const listsUrl = 'localhost:3000/list'
 const validEmail = 'dude@gmail.com'
 
 const emailInputSelector = 'label:contains("Email")~div>input'
+const taskTitleInputSelector = 'label:contains("Title")~div>input'
+const taskCommentInputSelector = 'label:contains("Comment")'
 
 const mainContentSelector = '[class*="content-container"]'
 
+const listsNavItem = '[href="/list"]'
 const submitLoginButtonSelector = 'span:contains("Submit")'
 const addListButtonSelector = 'span:contains("Add List")'
+const confirmAddTaskButtonSelector = 'span:contains("Add Todo")'
 
 const addListModal = '[class="add-list-form"]'
+
+var timestamp = Date.now()
+
+var taskTitle = 'Do It On: '
 
 
 describe('Create a new list', function () {
@@ -34,14 +42,32 @@ describe('Create a new list', function () {
     })
 
     it('Go to Lists page', function () {
-        cy.visit(listsUrl)
-        cy.url()
-            .should('include',listsUrl)
+        cy.get(listsNavItem)
+            .click()
     })
 
     it('Start Add List modal', function () {
         cy.get(addListButtonSelector)
             .click()
+    })
+
+    it('Insert a task name', function () {
+        taskTitle = taskTitle + timestamp
+
+        cy.get(taskTitleInputSelector)
+            .type(taskTitle)
+            .should('have.value',taskTitle)
+    })
+
+    it('Confirm task create modal', function () {
+        cy.get(confirmAddTaskButtonSelector)
+            .click()
+    })
+
+    it('The task should be successfully created', function () {
+        cy.get(mainContentSelector).should(($div) => {
+            expect($div.get(0).innerText).to.contain(taskTitle)
+        })
     })
 
 })
