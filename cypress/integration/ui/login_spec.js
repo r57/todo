8c6d8ebf9f6a2e70798ce50033984d63c296252c
vpos2 <vpospisil@fevo.com>
2020-03-17 16:37:47 +0100

    further setup

--- cypress/integration/ui/login_spec.js
+++ cypress/integration/ui/login_spec.js
@@ -1,4 +1,3 @@
-const loginUrl = 'localhost:3000'
 const validEmail = 'dude@gmail.com'
 const invalidEmail = 'dude2@gmail.com'
 const incorrectPassword = 'jirkakara'
@@ -9,50 +8,33 @@ const submitButtonSelector = 'span:contains("Submit")'
 const mainContentSelector = '[class*="content-container"]'
 
 
-describe('Log-in to the app', function () {
+describe('User Login', function () {
 
-    it('Go to login page', function () {
-        cy.visit(loginUrl)
-        cy.url()
-            .should('include',loginUrl)
-    })
+    it(`User is logged in with an email ${validEmail} and on mainpage`, function () {
+        cy.visit('')
 
-    it('Input a valid email: #validEmail', function () {
         cy.get(emailInputSelector)
             .clear()
             .type(validEmail)
             .should('have.value',validEmail)
-    })
 
-    it('Submit the login info', function () {
         cy.get(submitButtonSelector)
             .click()
-    })
 
-    it('User is successfully logged in and is redirected to the mainpage', function () {
         cy.get(mainContentSelector).should(($div) => {
             expect($div.get(0).innerText).to.eq('This is a home component')
         })
     })
-})
 
 
-describe('Use non-whitelisted email for login', function () {
+    it(`Use non-whitelisted email ${invalidEmail} for login`, function () {
+        cy.visit('')
 
-    it('Go to login page', function () {
-        cy.visit(loginUrl)
-        cy.url()
-            .should('include',loginUrl)
-    })
-
-    it('Input an invalid email: #invalidEmail', function () {
         cy.get(emailInputSelector)
             .clear()
             .type(invalidEmail)
             .should('have.value',invalidEmail)
-    })
 
-    it('Submit the login info and get rekt with an alert window', function () {
         const stub = cy.stub()
 
         cy.on ('window:alert', stub)
@@ -60,33 +42,23 @@ describe('Use non-whitelisted email for login', function () {
             .click()
             .then(() => {
                 expect(stub.getCall(0)).to.be.calledWith('Wrong password dude/et')      
-            })  
+        })
     })
-})
 
 
-describe('Use invalid password', function () {
-
-    it('Go to login page', function () {
-        cy.visit(loginUrl)
+    it('Use invalid password', function () {
+        cy.visit('')
         cy.url()
-            .should('include',loginUrl)
-    })
 
-    it('Input a valid email: #validEmail', function () {
         cy.get(emailInputSelector)
             .clear()
             .type(validEmail)
             .should('have.value',validEmail)
-    })
 
-    it('Input an incorrect password', function () {
         cy.get(passwordInputSelector)
             .clear()
             .type(incorrectPassword)
-    })
 
-    it('Submit the login info and get rekt with an alert window', function () {
         const stub = cy.stub()
 
         cy.on ('window:alert', stub)
@@ -95,5 +67,5 @@ describe('Use invalid password', function () {
             .then(() => {
                 expect(stub.getCall(0)).to.be.calledWith('Wrong password dude/et')      
             })  
-    })
+        })
 })
--- cypress/integration/ui/tasks_spec.js
+++ cypress/integration/ui/tasks_spec.js
@@ -1,4 +1,3 @@
-const loginUrl = 'localhost:3000'
 const listsUrl = 'localhost:3000/list'
 
 const validEmail = 'dude@gmail.com'
@@ -13,60 +12,114 @@ const listsNavItem = '[href="/list"]'
 const submitLoginButtonSelector = 'span:contains("Submit")'
 const addListButtonSelector = 'span:contains("Add List")'
 const confirmAddTaskButtonSelector = 'span:contains("Add Todo")'
+const deleteTaskButtonSelector = '[title="Remove list"]'
+const confirmButtonSelector = 'span:contains("Yes")'
+const cancelButtonSelector = 'span:contains("Cancel")'
+
 
 const addListModal = '[class="add-list-form"]'
+const deleteListModal = '[class="modal-inner"]'
 
-var timestamp = Date.now()
+var taskTitle = ''
+var taskTitleSelector = `[class="MuiCardContent-root"]>h3:contains("${taskTitle}")`
 
-var taskTitle = 'Do It On: '
+// TODO add beforeEach - login
+// TODO add beter taskTitle creation handling
+// TODO add after hook - delete all standing tasks
 
 
-describe('Create a new list', function () {
+describe('Tasks Manipulation', function () {
 
-    it('Go to login page', function () {
-        cy.visit(loginUrl)
-        cy.url()
-            .should('include',loginUrl)
-    })
+    beforeEach('User is successfully logged in', function () {
 
-    it('Input a valid email: #validEmail', function () {
+        cy.visit('')
+
         cy.get(emailInputSelector)
             .clear()
             .type(validEmail)
             .should('have.value',validEmail)
-    })
 
-    it('Submit the login info', function () {
         cy.get(submitLoginButtonSelector)
             .click()
     })
 
-    it('Go to Lists page', function () {
+
+    after('Make sure all tasks are deleted', function () {
         cy.get(listsNavItem)
             .click()
+        
+        cy.get(deleteTaskButtonSelector)
+            .each(($el) => {
+                $el.click()
+                cy.get(deleteListModal).within(() => {
+                    cy.get(confirmButtonSelector)
+                        .click()
+                })
+            })
+        
+        cy.get(mainContentSelector)
+            cy.get(mainContentSelector).should(($div) => {
+                expect($div.get(0).innerText).to.contain('No Todo lists available.')
+                expect($div.get(0).innerText).to.not.contain('Do It On: ')
+            })
     })
 
-    it('Start Add List modal', function () {
-        cy.get(addListButtonSelector)
+
+    it('User can create a new task', function () {
+        taskTitle = 'Do It On: ' + Date.now()
+
+        cy.get(listsNavItem)
             .click()
-    })
 
-    it('Insert a task name', function () {
-        taskTitle = taskTitle + timestamp
+        cy.get(addListButtonSelector)
+            .click()       
 
         cy.get(taskTitleInputSelector)
             .type(taskTitle)
             .should('have.value',taskTitle)
-    })
 
-    it('Confirm task create modal', function () {
         cy.get(confirmAddTaskButtonSelector)
             .click()
-    })
 
-    it('The task should be successfully created', function () {
         cy.get(mainContentSelector).should(($div) => {
             expect($div.get(0).innerText).to.contain(taskTitle)
+        })
+    })
+
+    it('User can delete a list', function () {
+        taskTitle = 'Do It On: ' + Date.now()
+
+        cy.get(listsNavItem)
+            .click()
+
+        cy.get(addListButtonSelector)
+            .click()      
+
+        cy.get(taskTitleInputSelector)
+            .type(taskTitle)
+            .should('have.value',taskTitle)
+
+        cy.get(confirmAddTaskButtonSelector)
+            .click()
+
+        cy.get(mainContentSelector).should(($div) => {
+            expect($div.get(0).innerText).to.contain(taskTitle)
+        })
+
+        cy.get(`[class="MuiCardContent-root"]>h3:contains("${taskTitle}")`)
+            .parent()
+            .parent().within(() => {
+                cy.get(deleteTaskButtonSelector)
+                    .click()
+        })
+
+        cy.get(deleteListModal).within(() => {
+            cy.get(confirmButtonSelector)
+                .click()
+        })
+
+        cy.get(mainContentSelector).should(($div) => {
+            expect($div.get(0).innerText).to.not.contain(taskTitle)
         })
     })
 
