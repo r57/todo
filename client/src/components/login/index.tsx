82abed21c0bb0bab68f807a9ad2c9dbdc397b8b5
Ivan Sivak <sivaki@seznam.cz>
2020-03-19 15:02:43 +0100

    lint fix

--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -1,34 +1,33 @@
-import React, { Component } from 'react'
-import ContentComponent from '../content/content.component'
-import SignUp from './signUp/signUp'
-import { UserAuthService } from '../../modules/services/user-auth.service'
-import './login.scss'
+import React, { Component } from 'react';
+import { UserAuthService } from '../../modules/services/user-auth.service';
+import ContentComponent from '../content/content.component';
+import './login.scss';
+import SignUp from './signUp/signUp';
 
-import { TextField, Button, Avatar } from '@material-ui/core'
-import Link from '@material-ui/core/Link'
-import Grid from '@material-ui/core/Grid'
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import { Avatar, Button, TextField } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 interface State {
-  email: string,
-  password: string,
-  signUp: boolean
+  email: string;
+  password: string;
+  signUp: boolean;
 }
 
 export default class LoginComponent extends Component<{}, State> {
-
   initialState() {
     this.setState({
       email: 'dude@gmail.com',
       password: '1234',
-      signUp: false
-    })
+      signUp: false,
+    });
   }
 
   componentWillMount() {
-    this.initialState()
+    this.initialState();
   }
 
   checkEmail() {
@@ -39,89 +38,79 @@ export default class LoginComponent extends Component<{}, State> {
   }
 
   showSignUp() {
-    this.setState({signUp: !this.state.signUp})
+    this.setState({ signUp: !this.state.signUp });
   }
 
   render() {
     return (
-      <ContentComponent className="center">
-        <Container component="main" maxWidth="xs">
+      <ContentComponent className='center'>
+        <Container component='main' maxWidth='xs'>
           <div className='login-form'>
             {!this.state.signUp ? (
               <UserAuthService.Consumer>
-              {state => (
-                <div className="fade-in">
-                  <div className="login-label-box">
-                    <Avatar className="avatar">
-                      <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5" align="center">
-                      Login
-                    </Typography>
+                {state => (
+                  <div className='fade-in'>
+                    <div className='login-label-box'>
+                      <Avatar className='avatar'>
+                        <LockOutlinedIcon />
+                      </Avatar>
+                      <Typography component='h1' variant='h5' align='center'>
+                        Login
+                      </Typography>
+                    </div>
+                    <form
+                      onSubmit={evt => {
+                        evt.preventDefault();
+                        state.setState(this.checkEmail());
+                      }}>
+                      <TextField
+                        variant='outlined'
+                        required={true}
+                        fullWidth={true}
+                        id='email'
+                        label='Email Address'
+                        name='email'
+                        autoComplete='email'
+                        onChange={val => this.setState({ email: val.target.value })}
+                        value={this.state.email}
+                      />
+                      <TextField
+                        variant='outlined'
+                        required={true}
+                        fullWidth={true}
+                        name='password'
+                        label='Password'
+                        type='password'
+                        id='password'
+                        value={this.state.password}
+                        onChange={val => this.setState({ password: val.target.value })}
+                      />
+                      <Button type='submit' fullWidth={true} variant='contained' color='primary'>
+                        Submit
+                      </Button>
+                      <Link
+                        href='#'
+                        variant='body2'
+                        onClick={() => {
+                          this.showSignUp();
+                        }}>
+                        Don't have an account?
+                      </Link>
+                    </form>
                   </div>
-                  <form
-                    onSubmit={evt => {
-                      evt.preventDefault()
-                      state.setState(this.checkEmail())
-                    }}>
-                    <TextField
-                      variant="outlined"
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-                      onChange={val => this.setState({ email: val.target.value })}
-                      value={this.state.email}
-                    />
-                    <TextField
-                      variant="outlined"
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      value={this.state.password}
-                      onChange={val =>
-                        this.setState({ password: val.target.value })
-                      }
-                    />
-                    <Button 
-                      type='submit'
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                    >
-                      Submit
-                    </Button>
-                    <Link 
-                      href="#"
-                      variant="body2"
-                      onClick={() => {
-                        this.showSignUp()
-                      }}
-                    >
-                      Don't have an account?
-                    </Link>
-                  </form>
-                </div>
-              )}
+                )}
               </UserAuthService.Consumer>
-            )
-            : (
-              <div className="fade-in">
+            ) : (
+              <div className='fade-in'>
                 <SignUp />
-                <Grid container justify="center">
-                  <Grid item>
+                <Grid container={true} justify='center'>
+                  <Grid item={true}>
                     <Link
-                      href="#"
-                      variant="body2"
+                      href='#'
+                      variant='body2'
                       onClick={() => {
-                        this.showSignUp()
-                      }}
-                    >
+                        this.showSignUp();
+                      }}>
                       Already have an account? Sign in
                     </Link>
                   </Grid>
--- client/src/components/login/signUp/signUp.tsx
+++ client/src/components/login/signUp/signUp.tsx
@@ -1,127 +1,121 @@
-import React, { useState } from 'react'
-import gql from 'graphql-tag'
-import { useMutation } from '@apollo/react-hooks'
-import '../login.scss'
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import React, { useState } from 'react';
+import '../login.scss';
 
-import Avatar from '@material-ui/core/Avatar'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 const SignUp = () => {
-
-  const [name, setName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const SIGNUP = gql`
-    mutation signUp($name: String!, $lastName: String!,  $email: String!, $password: String!) {
-    signUp(name: $name, lastName: $lastName, email: $email, password: $password) {
-      token
-      user {
-        name
+    mutation signUp($name: String!, $lastName: String!, $email: String!, $password: String!) {
+      signUp(name: $name, lastName: $lastName, email: $email, password: $password) {
+        token
+        user {
+          name
+        }
       }
     }
-  }
-  `
-  const [signUp] = useMutation(SIGNUP)
+  `;
+  const [signUp] = useMutation(SIGNUP);
 
   return (
     <>
-      <div className="login-label-box">
-        <Avatar className="avatar">
+      <div className='login-label-box'>
+        <Avatar className='avatar'>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5" align="center">
+        <Typography component='h1' variant='h5' align='center'>
           Sign up
         </Typography>
       </div>
-      <form 
-      onSubmit={e => {
-      e.preventDefault()
-      signUp({
-        variables: {
-          name: name,
-          lastName: lastName,
-          email: email,
-          password: password
-        }
-      })
-    }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          signUp({
+            variables: {
+              name,
+              lastName,
+              email,
+              password,
+            },
+          });
+        }}>
+        <Grid container={true} spacing={2}>
+          <Grid item={true} xs={12} sm={6}>
             <TextField
-              autoComplete="fname"
-              name="firstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              autoFocus
+              autoComplete='fname'
+              name='firstName'
+              variant='outlined'
+              required={true}
+              fullWidth={true}
+              id='firstName'
+              label='First Name'
+              autoFocus={true}
               onChange={e => {
-                setName(e.target.value)
+                setName(e.target.value);
               }}
             />
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item={true} xs={12} sm={6}>
             <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="lname"
+              variant='outlined'
+              required={true}
+              fullWidth={true}
+              id='lastName'
+              label='Last Name'
+              name='lastName'
+              autoComplete='lname'
               onChange={e => {
-                setLastName(e.target.value)
+                setLastName(e.target.value);
               }}
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item={true} xs={12}>
             <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              variant='outlined'
+              required={true}
+              fullWidth={true}
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
               onChange={e => {
-                setEmail(e.target.value)
+                setEmail(e.target.value);
               }}
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item={true} xs={12}>
             <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+              variant='outlined'
+              required={true}
+              fullWidth={true}
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
               onChange={e => {
-                setPassword(e.target.value)
+                setPassword(e.target.value);
               }}
             />
           </Grid>
         </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-        >
+        <Button type='submit' fullWidth={true} variant='contained' color='primary'>
           Sign Up
         </Button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
